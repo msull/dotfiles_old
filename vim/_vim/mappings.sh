5fddfa6e8da47cd22204548888fc15922dffd23d
Sully Sullenberger <sully@sadburger.com>
2015-12-30 09:25:16 -0800

    Commit

--- vim/_vim/mappings.sh
+++ vim/_vim/mappings.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ag "noremap" ~/.vim --ignore mappings.sh --nogroup --nocolor | cut -d' ' -f2- | sort
+ag "noremap" ~/.vim --ignore mappings.sh --ignore plug.vim --nogroup --nocolor | cut -d' ' -f2- | sort
