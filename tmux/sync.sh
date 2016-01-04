a0d329ea6d47fff9df3b52fad10255c2bab6db65
Sully Sullenberger <sully@sadburger.com>
2016-01-04 15:21:43 -0800

    standard commit

--- tmux/sync.sh
+++ tmux/sync.sh
@@ -4,4 +4,5 @@ echo 'Syncing tmux settings'
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # NOW=$(date +"%m_%d_%Y")
-rsync -r --exclude sync.sh ${DIR}/ ~/ 
+# rsync -r --exclude sync.sh ${DIR}/ ~/ 
+cp -f ${DIR}/_tmux.conf ~/.tmux.conf
