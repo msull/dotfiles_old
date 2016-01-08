2fe850a052a239a46d03dc48befc1083448f02f1
Sully Sullenberger <sully@sadburger.com>
2016-01-08 20:42:07 +0000

    standard commit

--- tmux/sync.sh
+++ tmux/sync.sh
@@ -2,6 +2,14 @@
 
 echo 'Syncing tmux settings'
 
+mkdir -p ~/.tmux_plugins
+
+if [ ! -e ~/.tmux_plugins/tmux-resurrect/resurrect.tmux ]; then
+    git clone https://www.github.com/tmux-plugins/tmux-resurrect ~/.tmux_plugins/tmux-resurrect
+else
+    git -C ~/.tmux_plugins/tmux-resurrect pull
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # NOW=$(date +"%m_%d_%Y")
 # rsync -r --exclude sync.sh ${DIR}/ ~/ 
