fdfba2eeb5160a6ee4d1a1932427d8cb72c39fbe
Sully Sullenberger <sully@sadburger.com>
2016-01-11 12:36:18 -0800

    standard commit

--- tmux/sync.sh
+++ tmux/sync.sh
@@ -14,3 +14,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # NOW=$(date +"%m_%d_%Y")
 # rsync -r --exclude sync.sh ${DIR}/ ~/ 
 cp -f ${DIR}/_tmux.conf ~/.tmux.conf
+cp -f ${DIR}/_tmux-old-mouse.conf ~/.tmux-old-mouse.conf
+cp -f ${DIR}/_tmux-new-mouse.conf ~/.tmux-new-mouse.conf
